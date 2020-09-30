d173d02cc9eb35f4d9a7cd967528c9ccd1b1e639
Julian Rocha <jules.r.rocha@gmail.com>
2020-09-29 20:48:07 -0700

    first commit

---
+++ scraper.py
@@ -0,0 +1,10 @@
+"""
+Mon-Fri 7AM-8PM
+Sat-Sun 10AM-5PM
+
+- Go through each lower day and check for availibility
+
+
+URL: https://activeliving.uvic.ca/Program/GetProducts?classification=4f3850f7-a86f-45f3-9c02-b2ce15fc054c
+
+"""
